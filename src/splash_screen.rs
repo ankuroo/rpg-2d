497b40fb7bad61074d06a12328f4816b9abe9d01
Ankur Mahto <ankurmahto1@gmail.com>
2025-08-27 22:31:37 +0100

    Add 'Initial' state for startup wait and apply ease function to fade animation.

--- src/splash_screen.rs
+++ src/splash_screen.rs
@@ -5,6 +5,7 @@ pub struct SplashScreen;
 
 #[derive(Component)]
 pub enum SplashState {
+    Initial(Timer),
     FadeIn(Timer),
     Wait(Timer),
     FadeOut(Timer),
@@ -23,35 +24,47 @@ impl Plugin for SplashScreenPlugin {
 
 }
 
+fn ease_fade(t: f32) -> f32 {
+    t * t * (3.0 - 2.0 * t) // smoothstep
+}
+
+
 fn fade_splash(
     time: Res<Time>,
     mut splash_query: Query<(&mut SplashState, &mut ImageNode), With<SplashScreen>>
 ) {
+
     if let Ok((mut state, mut image)) = splash_query.single_mut() {
 
         match &mut *state {
+
+            SplashState::Initial(x) => {
+                x.tick(time.delta());
+                if x.finished() {
+                    *state = SplashState::FadeIn(Timer::from_seconds(2.0, TimerMode::Once));
+                }
+            },
+
             SplashState::FadeIn(x) => {
                 x.tick(time.delta());
-                image.color.set_alpha(x.fraction());
+                image.color.set_alpha(ease_fade(x.fraction()));
                 if x.finished() {
                     *state = SplashState::Wait(Timer::from_seconds(3.0, TimerMode::Once));
                 }
             },
+
             SplashState::Wait(x) => {
                 x.tick(time.delta());
                 if x.finished() {
-                    *state = SplashState::FadeOut(Timer::from_seconds(1.0, TimerMode::Once));
+                    *state = SplashState::FadeOut(Timer::from_seconds(2.0, TimerMode::Once));
                 }
-
             },
+
             SplashState::FadeOut(x) => {
                 x.tick(time.delta());
-                image.color.set_alpha(1.0 - x.fraction());
-
+                image.color.set_alpha(ease_fade(1.0 - x.fraction()));
             },
-            _ => {
 
-            }
         }
     }
 }
@@ -76,7 +89,7 @@ fn setup_splash(
     .with_children(|parent| {
         parent.spawn((
             SplashScreen,
-            SplashState::FadeIn(Timer::from_seconds(1.0, TimerMode::Once)),
+            SplashState::Initial(Timer::from_seconds(2.0, TimerMode::Once)),
             ImageNode {
                 image: asset_server.load("splash.png"),
                 color: Color::linear_rgba(1.0, 1.0, 1.0, 0.0),
